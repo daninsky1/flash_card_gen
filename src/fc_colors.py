a46fa510439183e00f4aade5de740fd34f2be309
Daniel Santos <daninsky@protonmail.com>
2021-07-01 13:41:58 -0300

    Added fc_colours.py for colours constants

---
+++ src/fc_colors.py
@@ -0,0 +1,76 @@
+colors = {
+    "Blue":
+        (("#52B1FF", "#156CB3", "#38A6FF"), ("#FFFFFF", "#FFFFFF")),
+    "DarkGreen":
+        (("#4B8045", "#28801F", "#40CC31"), ("#FFFFFF", "#FFFFFF")),
+    "Pink":
+        (("#FFBDE8", "#9F4580", "#FF70CD"), ("#6B2E56", "#FFFFFF")),
+    "DirtYellow_Purple":
+        (("#AEB320", "#390966", "#6C20B3"), ("#FFFFFF", "#FFFFFF")),
+    "Orange":
+        (("#FF7536", "#B33F0A", "#FF621B"), ("#FFFFFF", "#FFFFFF")),
+    "Yellow":
+        (("#F2F27B", "#CCCC58", "#FFFBA0"), ("#000000", "#000000")),
+    "DarkBlue":
+        (("#260101", "#033E8C", "#0455BF"), ("#FFFFFF", "#FFFFFF")),
+    "Black_DarkSlateGray_SlateGray":
+        (("#000000", "#2F4F4F", "#708090"), ("#FFFFFF", "#FFFFFF")),
+    "Green_Blue":
+        (("#448C30", "#3068D9", "#82B8D9"), ("#FFFFFF", "#FFFFFF")),
+    "Pink_Blue":
+        (("#F25CA2", "#0433BF", "#0B9ED9"), ("#FFFFFF", "#FFFFFF")),
+    "Brown_Purple":
+        (("#2B190E", "#38024D", "#A457BF"), ("#FFFFFF", "#FFFFFF")),
+    "Banana":
+        (("#FFDF00", "#FFF249", "#FBFF87"), ("#402401", "#402401")),
+    "BananaPapayaSmoothie":
+        (("#F28D77", "#F29A2E", "#F2C335"), ("#FFFFFF", "#FFFFFF")),
+    "DarkPurple_Orange":
+        (("#150940", "#F29849", "#F2C849"), ("#FFFFFF", "#150940")),
+    "Orange_Lime":
+        (("#F2AC29", "#C8F230", "#4FBF30"), ("#000000", "#000000")),
+    "Lime":
+        (("#4FBF30", "#C8F230", "#4FBF30"), ("#000000", "#000000")),
+    "DarkBrown_DarkRed":
+        (("#40342D", "#BF7154", "#D97E6A"), ("#FFFFFF", "#FFFFFF")),
+    "AquaGreen":
+        (("#04708D", "#048B7E", "#05BF7D"), ("#FFFFFF", "#FFFFFF")),
+    "Gold":
+        (("#BF7E04", "#D9A91A", "#F2D230"), ("#FFFFFF", "#592B02")),
+    "RedPassion":
+        (("#8C0428", "#D9486E", "#F277A4"), ("#FFFFFF", "#FFFFFF")),
+    "MonoBlue":
+        (("#002559", "#405B82", "#8092AC"), ("#FFFFFF", "#FFFFFF")),
+    "LightPink_LightPurple":
+        (("#FFC4DD", "#F4D1FF", "#D1C4FF"), ("#000000", "#000000")),
+    "DirtGreenCamouflage":
+        (("#395944", "#547346", "#85A665"), ("#FFFFFF", "#FFFFFF")),
+    "Brazil":
+        (("#002776", "#009C3B", "#FFDF00"), ("#FFFFFF", "#002776")),
+    "WaterMelon":
+        (("#02733E", "#BF0413", "#F21B07"), ("#FFFFFF", "#FFFFFF")),
+    "Forest":
+        (("#733F2D", "#698C35", "#87BF34"), ("#FFFFFF", "#FFFFFF")),
+    "Lime2":
+        (("#38A67E", "#6AD97B", "#BCF26B"), ("#FFFFFF", "#000000")),
+    "Dark":
+        (("#05070D", "#101C26", "#283540"), ("#FFFFFF", "#FFFFFF")),
+    "Silver":
+        (("#808080", "#BFBFBF", "#E6E6E6"), ("#000000", "#000000")),
+    "White":
+        (("#FFFFFF", "#E6E6E6", "#FFFFFF"), ("#000000", "#000000")),
+    "DarkBlue_DarkCream":
+        (("#203140", "#D9A566", "#F2D57E"), ("#FFFFFF", "#000000")),
+    "DarkCream":
+        (("#BF8756", "#D98E32", "#F2D57E"), ("#000000", "#000000")),
+    "SpongeBob":
+        (("#0457A0", "#AEAD0D", "#FFF56C"), ("#FFFFFF", "#000000")),
+    "MonoDarkRed":
+        (("#BF7373", "#A63838", "#BF414C"), ("#FFFFFF", "#FFFFFF")),
+    "RedSand":
+        (("#A66D6D", "#D99791", "#F2B199"), ("#000000", "#000000")),
+    "DarkDirt":
+        (("#402B12", "#593F1E", "#8C704D"), ("#FFFFFF", "#FFFFFF")),
+    "Bronze":
+        (("#BF978E", "#BF5E49", "#D9857E"), ("#FFFFFF", "#FFFFFF")),
+}
